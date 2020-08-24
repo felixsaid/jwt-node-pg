7b98da3196c933714ff550869c986838be07b1b3
felixsaid <kiambafelix@yahoo.com>
2020-08-25 01:43:57 +0300

    Completed client-side

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+//components
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  async function isAuth() {
+    try {
+      const response = await fetch("http://localhost:5000/v4/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    isAuth();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useState, useEffect } from "react";
+
+const Dashboard = ({ setAuth }) => {
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/v4/dashboard", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      setName(parseRes.user_name);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const logOut = (e) => {
+    e.preventDefault();
+
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+
+  return (
+    <Fragment>
+      <h1 className="text-center my-5">Dashboard</h1>
+      <h4 className="text-center">Welcome {name}</h4>
+
+      <div className="row">
+        <div className="col text-center my-5">
+          <button className="btn btn-primary" onClick={(e) => logOut(e)}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Dashboard;
